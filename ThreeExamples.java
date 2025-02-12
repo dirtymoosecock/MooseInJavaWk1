9aa3971cb23254b7a0a0a6d56b3bc6051c5258db
dirtymoosecock <tonyrg007@gmail.com>
2025-02-12 10:20:17 -0500

    Add files via upload

---
+++ ThreeExamples.java
@@ -0,0 +1,16 @@
+public class ThreeExamples {
+    public static void main(String[] args)
+    {
+        System.out.println("\tDog Show Winners");
+        System.out.println("Place \t1\t2\t3\t4\t5\t6");
+        System.out.println("Dog: #\t76\t32\t18\t47\t11\t29");
+
+        System.out.println("\n");
+
+        System.out.println("Q\"yz89\"Ja4\"tp");
+
+        System.out.println("\n");
+
+        System.out.println("Anderson\t3452\nBoyton\t55344\nCarrington\t3677\nHouston\t5988\nMcDougal\t5233\nRobertson\t3623\nStryker\t5112");
+    }
+}
